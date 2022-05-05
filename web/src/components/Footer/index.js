7a15f15b237b8a8e1e0d5983a1d921aa6d5d8f5b
Samuel Porto <samuelpporto@gmail.com>
2022-05-05 00:11:36 -0300

    footer adicionado

---
+++ web/src/components/Footer/index.js
@@ -0,0 +1,10 @@
+export function Footer() {
+  return (
+    <footer className="max-w-6xl m-auto">
+      <div className="flex justify-between">
+        <p><a href="http://www.utfpr.edu.br">Universidade Tecnológica Federal do Paraná</a></p>
+		  <p>© All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
