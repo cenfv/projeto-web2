6ee3b323ec1002b5ce3ddf5ecd8ef5de1e0a9275
cenfv <59135587+cenfv@users.noreply.github.com>
2022-05-05 05:03:04 -0300

    Delete backend directory

--- backend/src/controllers/userController.js
+++
@@ -1,7 +0,0 @@
-const User = require("../models/User");
-
-exports.createUser = async (firstName, lastName, email, password, gender) => {
-    const user = new User({ firstName, lastName, email, password, gender });
-    await user.save();
-    console.log(user);
-}
