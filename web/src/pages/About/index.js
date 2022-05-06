9daf2ae9c6f54fd93d0423d253392b870b4633c0
Samuel Porto <samuelpporto@gmail.com>
2022-05-05 23:46:22 -0300

    Pagina sobre criada

--- web/src/pages/About/index.js
+++ web/src/pages/About/index.js
@@ -1,13 +1,71 @@
 import { Footer } from "../../components/Footer";
 import { Navbar } from "../../components/Navbar";
+import { ClipboardCheckIcon, GlobeIcon, LightningBoltIcon, CashIcon } from '@heroicons/react/outline'
 
+const features = [
+  {
+    name: 'Maior rendimento',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: LightningBoltIcon,
+  },
+  {
+    name: 'Totalmente gratuito',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: CashIcon,
+  },
+  {
+    name: 'Leaderboard',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: GlobeIcon,
+  },
+  {
+    name: 'Correção automática',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: ClipboardCheckIcon,
+  },
+]
 
 export function About() {
-    return (
-      <div>
-        <Navbar />
+  return (
+    <div>
+      <Navbar />
 
-    <div className="mb-5 fixed left-0 bottom-0 w-full">
+      <div className="max-w-6xl mx-auto mt-10">
+        <div className="lg:text-center">
+          <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
+            Sobre nós
+          </h2>
+          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            Uma nova maneira de se estudar
+          </p>
+          <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam
+            voluptatum cupiditate veritatis in accusamus quisquam.
+          </p>
+        </div>
+
+				<div className="mt-16">
+          <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+            {features.map((feature) => (
+              <div key={feature.name} className="relative">
+                <dt>
+                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                    <feature.icon className="h-6 w-6" aria-hidden="true" />
+                  </div>
+                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">{feature.name}</p>
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500">{feature.description}</dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
+
+      <div className="mb-5 fixed left-0 bottom-0 w-full">
         <Footer />
       </div>
     </div>
