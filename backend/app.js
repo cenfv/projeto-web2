dd8fbfc24051acea26c52f922917fd905169cccc
cenfv <59135587+cenfv@users.noreply.github.com>
2022-05-12 02:13:31 -0300

    Pequenas correções no back

--- backend/app.js
+++ backend/app.js
@@ -1,47 +1,48 @@
-var createError = require('http-errors');
-var express = require('express');
-var cors = require('cors');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var cors = require("cors");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./src/routes/index');
-var userRouter = require('./src/routes/user');
-var authRouter = require('./src/routes/auth');
+var indexRouter = require("./src/routes/index");
+var userRouter = require("./src/routes/user");
+var authRouter = require("./src/routes/auth");
 
 var app = express();
-var mongodbConnection = require('./modules/mongodb/mongodb-connection');
+var mongodbConnection = require("./src/database/mongodb/mongodb-connection");
+//var mongodbConnection = require('./modules/mongodb/mongodb-connection');
 // view engine setup
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
+
 mongodbConnection();
 
-
-app.use('/', indexRouter);
-app.use('/user', userRouter);
-app.use('/auth', authRouter);
+app.use("/", indexRouter);
+app.use("/user", userRouter);
+app.use("/auth", authRouter);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
